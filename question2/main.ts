5e94dd47ebc811f5299d6c57c2db8a64c6c1987a
DharaGelani <79169035+DharaGelani@users.noreply.github.com>
2021-02-16 16:03:10 -0500

    Update main.ts

--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,30 @@
-export function findOutlier(integers: number[]): number {
-    
+  public static int Find(int[] integers)
+  {
+  int numerOfOdds = 0;
+              int numberOfEvens = 0;
+              int integerOutlier;
+
+
+                for(int i = 0; i < integers.Length ;i++){
+                  if ( integers[i]%2 == 0){
+                    numberOfEvens++;
+                  }else{
+                    numerOfOdds++;
+                  }  
+                }
+
+                if ( numberOfEvens > numerOfOdds){
+                  integerOutlier = 1;    
+                }else{
+                  integerOutlier = 0;
+                }
+
+               for(int i = 0; i < integers.Length; i++){
+  if (Math.Abs(integers[i]%2) == integerOutlier){
+    return integers[i];
+  }
+}
+
+              return 0;
+  
   }
